a368bc1f0131a29f781d1d7b4873ad021b576c02
Oleh Palianytsia <oleh.palianytsia@gmail.com>
2016-03-20 12:10:07 +0200

    Initial upload

---
+++ scripts/poke-client.js
@@ -0,0 +1,14 @@
+var PokedexRoot = React.createClass({
+  render: function() {
+    return (
+      <div>
+        Hello world
+      </div>
+    );
+  }
+});
+
+ReactDOM.render(
+  <PokedexRoot/>,
+  document.getElementById("container")
+);
